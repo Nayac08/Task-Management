3aa7333658afba86a47f4b43033ddb7fc6733e98
Narumedsr Pitayachamrat <narumedsr.p@gmail.com>
2025-04-10 14:34:36 +0700

    add maininterface

--- src/app/main.java
+++ src/app/main.java
@@ -22,7 +22,7 @@ public class Main extends Application {
         primaryStage.setTitle("Task Management");
         primaryStage.setResizable(false);
         primaryStage.getIcons().add(new Image("/Logo.png"));
-        primaryStage.setScene(new Scene(root, 1280, 720));
+        primaryStage.setScene(new Scene(root, 1420, 720));
         primaryStage.show();
     }
 
